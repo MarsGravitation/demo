aa453ce77bd0312e5c4d964ca12c805c36d950fd
chengxudong <18435202728@163.com>
2020-07-17 22:24:34 +0800

    2020-07-17 23:00:00

--- study-day01/jdbc/src/main/java/com/microwu/cxd/jdbc/jdbc/JdbcTest.java
+++ study-day01/jdbc/src/main/java/com/microwu/cxd/jdbc/jdbc/JdbcTest.java
@@ -1,6 +1,7 @@
 package com.microwu.cxd.jdbc.jdbc;
 
 import java.sql.*;
+import java.util.Properties;
 
 /**
  * Description:
@@ -13,19 +14,62 @@ import java.sql.*;
  */
 public class JdbcTest {
 
+    private static final String URL = "jdbc:mysql://192.168.201.128:3306/test?useUnicode=true";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+    private static final String SQL = "select * from user";
+
     public static void main(String[] args) {
-        String url = "";
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
 
         try {
             // 1. 加载并注册 MySQL 的驱动
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+
             // 2. 根据特定的数据库连接URL，返回与次URL的所匹配的数据库驱动对象
-            Driver driver = DriverManager.getDriver(url);
+            Driver driver = DriverManager.getDriver(URL);
+
+            // 3. 传入参数，
+            Properties properties = new Properties();
+            properties.put("user", USER);
+            properties.put("password", PASSWORD);
+
+            // 4. 使用数据库驱动创建数据库连接池 Connection
+            connection = driver.connect(URL, properties);
+
+            // 5. 从数据库连接中获得 Statement 对象
+            statement = connection.createStatement();
+
+            // 6. 执行 SQL 语句，返回结果
+            resultSet = statement.executeQuery(SQL);
+
+            // 7. 处理结果，取出数据
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(2));
+            }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            // 8. 关闭资源，释放资源
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
